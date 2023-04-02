3765683a0e9a7d7400a1c0c053a21c84c5fa4dfb
ebube <aaronebube123@gmail.com>
2023-04-02 23:13:26 +0100

    update frontend

---
+++ frontend2/src/joiValidations/login.ts
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+export const schema = Joi.object({
+	username: Joi.string().alphanum().min(3).max(30).required().messages({
+		"string.base": `username should be a string`,
+		"string.empty": `username cannot be empty`,
+		"string.min": `username should have a minimum length of {#limit}`,
+		"string.max": `username should have a maximum length of {#limit}`,
+		"any.required": `username is a required field`,
+	}),
+	password: Joi.string()
+		.pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+		.required()
+		.messages({
+			"any.required": "password is a required field",
+		}),
+}).options({ abortEarly: true });
---
+++ frontend2/src/joiValidations/signUp.ts
@@ -0,0 +1,28 @@
+import Joi from "joi";
+
+export const schema = Joi.object({
+	username: Joi.string().alphanum().min(3).max(30).required().messages({
+		"string.base": `username should be a string`,
+		"string.empty": `username cannot be empty`,
+		"string.min": `username should have a minimum length of {#limit}`,
+		"string.max": `username should have a maximum length of {#limit}`,
+		"any.required": `username is a required field`,
+	}),
+	password: Joi.string()
+		.pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+		.required()
+		.messages({
+			"any.required": "password is a required field",
+		}),
+    email: Joi.string()
+		.email({
+			minDomainSegments: 2,
+			tlds: { allow: ["com", "net"] },
+		})
+		.required()
+		.messages({
+			"string.base": `email should be a string`,
+			"string.empty": `email cannot be empty`,
+			"any.required": `email is a required field`,
+		}),
+}).options({ abortEarly: true });
