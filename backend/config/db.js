9ccc644b853051e01f41a8041836ba50b4c358ae
ebube <aaronebube123@gmail.com>
2023-04-05 23:11:48 +0100

    for the life of me trying to fix CORS error

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,35 +1,35 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const dbConfig = {
-	HOST: "localhost",
-	USER: "postgres",
-	PASSWORD: "password",
-	DB: "postgres",
-	dialect: "postgres",
-	dialectOptions: {
-		ssl: true,
-	},
-};
-
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-	host: dbConfig.HOST,
-	dialect: dbConfig.dialect,
-	operatorsAliases: false,
-});
-
-// const sequelize = new Sequelize(process.env.DATABASE_URI, {
+// const dbConfig = {
+// 	HOST: "localhost",
+// 	USER: "postgres",
+// 	PASSWORD: "password",
+// 	DB: "postgres",
 // 	dialect: "postgres",
-// 	protocol: "postgres",
-// 	port: 5432,
-// 	pool: {
-// 		max: 5,
-// 		min: 0,
-// 		acquire: 30000,
-// 		idle: 10000,
-// 	},
 // 	dialectOptions: {
 // 		ssl: true,
 // 	},
+// };
+
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+// 	host: dbConfig.HOST,
+// 	dialect: dbConfig.dialect,
+// 	operatorsAliases: false,
 // });
+
+const sequelize = new Sequelize(process.env.DATABASE_URI, {
+	dialect: "postgres",
+	protocol: "postgres",
+	port: 5432,
+	pool: {
+		max: 5,
+		min: 0,
+		acquire: 30000,
+		idle: 10000,
+	},
+	dialectOptions: {
+		ssl: true,
+	},
+});
 module.exports = sequelize;
