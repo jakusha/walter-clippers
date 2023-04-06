2ee53f523f5fac621d09f471569a7c95ccf951b1
ebube <aaronebube123@gmail.com>
2023-04-06 23:10:38 +0100

    update cors url

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-	"https://cutzy-razors.vercel.app",
-	"https://cutzy-razors-bube-create.vercel.app",
-	"https://cutzy-razors-git-main-bube-create.vercel.app",
+	"https://walter-clippers.vercel.app",
 	"http://localhost:3000",
 	"http://127.0.0.1:5173",
 	"http://localhost:5173",
