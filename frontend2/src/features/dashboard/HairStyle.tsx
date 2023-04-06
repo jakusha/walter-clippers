7c61b57d4a273960023c4f6282c9277de9240cf0
ebube <aaronebube123@gmail.com>
2023-04-06 17:17:07 +0100

    feat: analytics routes

--- frontend2/src/features/dashboard/HairStyle.tsx
+++ frontend2/src/features/dashboard/HairStyle.tsx
@@ -35,7 +35,7 @@ const HairStyle = () => {
 		hairStyleId: "",
 	});
 	const dispatch = useAppDispatch();
-	const [createNewHairStyle] = useCreateNewHairStyleMutation();
+	const [createNewHairStyle, status] = useCreateNewHairStyleMutation();
 	const { data: hairStyleData } = useGetAllHairStylesQuery();
 	const [updateHairStyle] = useUpdateHairStyleMutation();
 	const [updating, setUpdating] = useState(false);
@@ -90,6 +90,11 @@ const HairStyle = () => {
 				if (result.error) {
 					setErroMsg(result.error.data.message);
 				} else {
+					setHairStyleValue({
+						name: "",
+						price: "",
+						hairStyleId: "",
+					})
 					setNewHairStyleModal(false);
 				}
 			} catch (error) {
@@ -107,6 +112,11 @@ const HairStyle = () => {
 				if (result.error) {
 					setErroMsg(result.error.data.message);
 				} else {
+					setHairStyleValue({
+						name: "",
+						price: "",
+						hairStyleId: "",
+					})
 					setNewHairStyleModal(false);
 				}
 			} catch (error) {
@@ -116,6 +126,21 @@ const HairStyle = () => {
 		// console.log(hairStyleValue, "SUBmiteed value");
 	}
 
+	let buttonEl;
+
+	if(status?.isLoading) {
+		buttonEl = <button className={`bg-teal-300 text-blue-4
+		p-2 px-8 text-xl my-4 font-semibold  mx-auto block w-max  flex gap-2`}>
+			<svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+      </svg>
+			loading...</button>
+	}else {
+buttonEl = <button className="bg-blue-4 text-white-2  flex items-center justify-center p-2 px-4 my-4 cursor-pointer mx-auto">
+									{updating ? "update" : "confirm"}
+								</button>
+	}
 	let content;
 	if (hairStyleData) {
 		content = hairStyleData?.hairStyles?.map(
@@ -179,9 +204,9 @@ const HairStyle = () => {
 						<path
 							fill="none"
 							stroke="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="32"
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="32"
 							d="M256 112v288M400 256H112"
 						/>
 					</svg>
@@ -247,9 +272,7 @@ const HairStyle = () => {
 										className="w-full capitalize py-1 border-2 border-blue-4 px-2"
 									/>
 								</div>
-								<button className="bg-blue-4 text-white-2  flex items-center justify-center p-2 px-4 my-4 cursor-pointer mx-auto">
-									{updating ? "update" : "confirm"}
-								</button>
+								{buttonEl}
 							</form>
 						</div>
 					</div>,
